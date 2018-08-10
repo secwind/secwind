8eed92b88231f7695f854c1020f7925ba7f2ccac
SecWind <36530139+secwind@users.noreply.github.com>
2018-08-10 11:31:05 +0700

    Add files via upload

---
+++ src/Context.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import Axios from '../node_modules/axios';
+
+
+const Context = React.createContext();
+const Reducer = (state, action) => {
+  switch (action.type) {
+    case 'Deleted_Contact':
+      return {
+        ...state,
+        contacts: state.contacts.filter(contact =>
+        contact.id !== action.payload)
+      }
+
+      case 'Add_Contact':
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts]
+      } 
+      
+      case 'Update_Contact':
+      return {
+        ...state,
+        contacts: state.contacts.map(contact =>
+          contact.id === action.payload.id ? 
+          (contact = action.payload ) : (contact)  
+        )
+          
+       
+      } 
+    default:
+      return state;  
+
+  }
+}
+
+export  class Provider extends Component {
+    state = {
+      contacts: [],
+      dispatch: action => this.setState(state =>
+      Reducer(state, action)  
+      ),
+    }
+
+    async componentDidMount(){
+      const res = await Axios.get('https://jsonplaceholder.typicode.com/users');
+      this.setState({ contacts: res.data })
+        
+ 
+    }
+
+    
+    render() {
+      return (
+        <Context.Provider value={this.state}>
+            {this.props.children}
+        </Context.Provider>
+      )
+    }
+  }
+
+export const Consumer = Context.Consumer;
+  
